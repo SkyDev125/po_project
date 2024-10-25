3efc4ab118a2c11ce4c633dba9f6f73176cf57a5
Sky <diogo_goncalves2004@hotmail.com>
2024-10-25 00:17:25 +0000

    updating google checks to remove some that we dont care much about.

--- linting/pmd/run_linter.sh
+++ linting/pmd/run_linter.sh
@@ -1,1 +1,1 @@
-./pmd-bin-7.6.0/bin/pmd.sh check --progress -d ../../hva -R rulesets/java/quickstart.xml -r report.out -f text --cache=pmd_cache
+./pmd-bin-7.6.0/bin/pmd.sh check --progress -d ../../hva -R rulesets/java/quickstart.xml -r report.out -f text --cache=pmd.cache
